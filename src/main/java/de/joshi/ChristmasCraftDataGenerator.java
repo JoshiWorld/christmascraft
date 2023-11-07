d99f177117750590f8999f1dab0503c8aeab2dd2
Joshua Stieber <joshua.stieber.1903@gmail.com>
2023-11-07 11:22:29 +0100

    basic files

---
+++ src/main/java/de/joshi/ChristmasCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package de.joshi;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChristmasCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
